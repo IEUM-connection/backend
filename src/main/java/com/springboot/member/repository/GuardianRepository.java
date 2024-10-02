38b2ad5dcac304f8289ed0f21418daa89173f4f7
m0x0m <165968962+wooil6@users.noreply.github.com>
2024-10-02 17:57:37 +0900

    Delete src/main directory

--- src/main/java/com/springboot/member/repository/GuardianRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.member.repository;
-
-import com.springboot.member.entity.Guardian;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GuardianRepository extends JpaRepository<Guardian, Long> {
-
-}
--- src/main/java/com/springboot/member/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.member.repository;
-
-import com.springboot.member.entity.Guardian;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository<Member> extends JpaRepository<Member, Long> {
-}
