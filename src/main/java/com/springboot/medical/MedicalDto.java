4965c2224a1f96d017691e39505d8aba5da6d927
hhwang1022 <hjd2110@naver.com>
2024-10-14 15:47:20 +0900

    클라에 필요없는 변수들 주석

--- src/main/java/com/springboot/medical/MedicalDto.java
+++ src/main/java/com/springboot/medical/MedicalDto.java
@@ -12,19 +12,18 @@ public class MedicalDto {
     @Setter
     @AllArgsConstructor
     public static class Response {
-        private int rnum;
+//        private int rnum;
         private double distance;
-        private double latitude;
-        private double longitude;
+//        private double latitude;
+//        private double longitude;
         // 병원분류명 ex)종합병원, 의원,...
         private String dutyDivName;
         private String dutyAddr;
         private String name;
-        private String hpid;
         private String dutyTel1;
-        private String dutyFax;
-        private Integer startTime;
-        private Integer endTime;
+//        private String dutyFax;
+//        private Integer startTime;
+//        private Integer endTime;
         // 운영상태
         private Integer isOpen;
     }
--- src/main/java/com/springboot/medical/MedicalMapper.java
+++ src/main/java/com/springboot/medical/MedicalMapper.java
@@ -12,8 +12,6 @@ import java.util.List;
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface MedicalMapper {
     default List<MedicalDto.Response> infoToResponseDto(JSONObject info){
-        System.out.println(info);
-
         List<MedicalDto.Response> responseList = new ArrayList<>();
 
         JSONObject response = info.getJSONObject("response");
@@ -24,24 +22,24 @@ public interface MedicalMapper {
         for (int i = 0; i < itemArray.length(); i++) {
             JSONObject item = itemArray.getJSONObject(i);
 
-            int rnum = item.getInt("rnum");
+//            int rnum = item.getInt("rnum");
             double distance = item.getDouble("distance");
-            double latitude = item.getDouble("latitude");
-            double longitude = item.getDouble("longitude");
+//            double latitude = item.getDouble("latitude");
+//            double longitude = item.getDouble("longitude");
             String dutyDivName = item.getString("dutyDivName");
             String dutyAddr = item.getString("dutyAddr");
             String dutyName = item.getString("dutyName");
-            String hpid = item.getString("hpid");
+//            String hpid = item.getString("hpid");
             String dutyTel1 = item.getString("dutyTel1");
-            String dutyFax = item.optString("dutyFax", ""); // Null일 수 있으므로 optString 사용
-            Integer startTime = item.getInt("startTime");
-            Integer endTime = item.getInt("endTime");
+//            String dutyFax = item.optString("dutyFax", ""); // Null일 수 있으므로 optString 사용
+//            Integer startTime = item.getInt("startTime");
+//            Integer endTime = item.getInt("endTime");
             Integer isOpen = -1;
             if(!item.isNull("dutyLvkl")){
                 isOpen = item.getInt("dutyLvkl");
             }
 
-            MedicalDto.Response medical = new MedicalDto.Response(rnum, distance, latitude, longitude, dutyDivName, dutyAddr, dutyName, hpid, dutyTel1, dutyFax, startTime, endTime, isOpen);
+            MedicalDto.Response medical = new MedicalDto.Response(distance, dutyDivName, dutyAddr, dutyName, dutyTel1, isOpen);
             responseList.add(medical);
         }
 
